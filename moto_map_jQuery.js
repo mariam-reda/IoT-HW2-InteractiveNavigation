b7892e509a0c7d70e4a0e4c80adb0a466759519c
mariam-reda <60571860+mariam-reda@users.noreply.github.com>
2020-11-01 20:41:38 +0400

    Update moto_map_jQuery,js

--- moto_map_jQuery.js
+++ moto_map_jQuery.js
@@ -1,12 +1,16 @@
 
 var mymap;
 var currentLocationCoordinates;
+var destinationLocationCoordinates;
+var currentLocationMarker;
 var accuracyRadiusCircle;
 var radiusDisplayedOnce = false;
 var control;
 
+var startingLocationMarker; //will mark the starting location (first detected location, vs. current location being tracked while moving)
+
 //---------------------------------------------
-//custom red icon (used to differentiate between starting location and destination (default blue icon))
+//custom red icon (used to differentiate between current location and destination (default blue icon))
 var redIcon = new L.Icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -14,6 +18,16 @@ var redIcon = new L.Icon({
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
+});
+
+//custom violet icon (used to differentiate starting location)
+var violetIcon = new L.Icon({
+	iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
+	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41]
 });
 
 //------------------------------------------------------------
@@ -44,21 +58,57 @@ window.onload = function() {
     //Geolocation - location found function
     function onLocationFound(e) 
     {
+        console.log("onLocationFound called.");
+        
         //accuracy radius of location detected
         var radius = e.accuracy / 8;
 
         //store current location coordinates
         currentLocationCoordinates = e.latlng;
 
-        //set marker at current location [uses red icon]
-        var currentLocationMarker = L.marker(currentLocationCoordinates, {icon: redIcon}).addTo(mymap).bindPopup("<b>Detected Current Location</b><br/>You are within " + radius + " meters from this point").openPopup();
+        //currentLocationMarker = L.marker(currentLocationCoordinates, {icon: redIcon}).addTo(mymap).bindPopup("<b>Detected Current Location</b><br/>You are within " + radius + " meters from this point").openPopup();
 
-        //display radius circle
+
+        //update display according to location read (first location detected; current tracked location w/o route set; updated current location when route set)
+        if (startingLocationMarker == null) //first location being detected -> add starting location marker (fixed violet marker)
+        {
+            startingLocationMarker = L.marker(currentLocationCoordinates, {icon: violetIcon}).addTo(mymap).bindPopup("<b>Detected Starting Location</b><br/>You have started within " + radius + " meters from this point").openPopup();
+            //starting marker is displayed throughout whole run
+        }
+        else if (startingLocationMarker != null && destinationLocationCoordinates == null)    //starting location already read, but no destination set -> just updating current location marker (tracking movement)
+        {
+            //create new current location marker or update coordinates of marker if existing
+            if (currentLocationMarker == null)
+            {   
+                currentLocationMarker = L.marker(currentLocationCoordinates, {icon: redIcon}).addTo(mymap).bindPopup("<b>Detected Current Location</b>").openPopup();   
+            }
+            else
+            {   
+                currentLocationMarker.setLatLng(currentLocationCoordinates);    
+            }
+            
+        }
+        else if (destinationLocationCoordinates != null)    //destination set -> update waypoints, route, and server with new current location
+        {
+            //if displayed, remove manually-placed current location marker (tracked current location marker will be placed by control object based on waypoints and route)
+            if (currentLocationMarker != undefined)
+            {   
+                mymap.removeLayer(currentLocationMarker);
+                currentLocationMarker = undefined;
+            }
+
+            //update waypoints and determine new route
+            routeMap(currentLocationCoordinates, destinationLocationCoordinates, "current");   //this function calls subsequent server updating functions
+            
+        } 
+        
+        //display radius circle (later removed when a destination is set)
         if (!radiusDisplayedOnce) 
         {   
             accuracyRadiusCircle = L.circle(currentLocationCoordinates, radius, {color: 'yellow', fillColor: 'yellow', fillOpacity: 0.2}).addTo(mymap); 
             radiusDisplayedOnce = true;
         }
+
         //set map view to be centered at current location
         mymap.setView(currentLocationCoordinates, 13);
     }
@@ -97,8 +147,8 @@ window.onload = function() {
             {   initializeControl();    }
 
             //pass destination coordinates to routeMap() function
-            var destination_latlong = e.latlng;
-            routeMap(destination_latlong);
+            destinationLocationCoordinates = e.latlng;
+            routeMap(currentLocationCoordinates, destinationLocationCoordinates, "destination");
         }
         else    //current location detection was blocked/error occurred
         {
@@ -123,7 +173,7 @@ function initializeControl()
             if (i == 0) //starting location marker
             {
                 locMarker.setIcon(redIcon);                             //starting location marker uses red icon
-                locMarker.bindPopup("Starting Location");
+                locMarker.bindPopup("Current Location");
             }
             else if (i == 1)    //destination location marker
             {
@@ -137,31 +187,39 @@ function initializeControl()
 
 //-------------------------------------------------------------
 //function sets waypoints for control object, calculating and displaying the route
-function routeMap(dest_latlong) 
+function routeMap(current_latlong, dest_latlong, updatedLocation)   
 {
     //update destination point and route
     console.log("routeMap() function called");
 
     //set the location waypoints (triggers route calculation and display)
-    control.setWaypoints([currentLocationCoordinates, dest_latlong]);
+    control.setWaypoints([current_latlong, dest_latlong]);
 
     //hide regenerated directions box
     $(".leaflet-routing-container").css("display", "none");
     //$(".leaflet-control-container").css("display", "none");
 
-    //post the updated locations to the server
-    postLocationsToServer(currentLocationCoordinates, dest_latlong);
+    //update server - POST/GET used depending on which location was updated (current = GET vs. destination = POST)
+    if (updatedLocation == "current")
+    {
+        sendGetRequestToServer(current_latlong, dest_latlong);
+    }
+    else    //updatedLocation == "destination"
+    {
+        postLocationsToServer(current_latlong, dest_latlong);   
+    }
+
 }
 
 //-------------------------------------------------------------
-//postLocationsToServer() - function executes POST requests to server
+//postLocationsToServer() - function executes POST requests to server (when new destination is set)
 function postLocationsToServer(startLoc, destinationLoc) 
 {
     console.log("Called postLocationsToServer() method.");
     console.log("StartLoc = " + startLoc + ", destinationLoc = " + destinationLoc);
 
-    $.post('http://localhost:1234',  // url
-        JSON.stringify({
+    $.post('http://localhost:1234',  // url of server
+        JSON.stringify({            // data sent in request
             startingCoordinates: startLoc,
             destinationCoordinates: destinationLoc,
         }),
@@ -171,4 +229,27 @@ function postLocationsToServer(startLoc, destinationLoc)
         }).fail((function(jqxhr, settings, ex) {            // failure to connect to server callback function
             alert("Error. Could not connect to server to post current and destination locations.\n" + ex);
         }));
+}
+
+//-------------------------------------------------------------------------
+//sendGetRequestToServer() - function executes GET requests to server to update locations (when changes occur in current postion)
+function sendGetRequestToServer(currentLoc, destinationLoc) 
+{
+    console.log("Called sendGetRequestToServer() method.");
+
+    $.get("http://localhost:1234",  // url of server
+        {                                               //data being sent (split lat and long to allow for encoding)
+            currentLocation_lat: currentLoc.lat, 
+            currentLocation_long: currentLoc.lng, 
+            destinationLocation_lat: destinationLoc.lat,
+            destinationLocation_long: destinationLoc.lng
+        },
+        function (data, textStatus, jqXHR) {              // success callback function
+          //alert('status: ' + textStatus + ', data: ' + data);
+          console.log('status: ' + textStatus + ', data from server: ' + data);
+    }).fail((function(jqxhr, settings, ex) {            // failure to connect to server callback function
+        alert("Error. Could not connect to server to send current and destination locations in GET request.\n" + ex);
+    }));
+
+    console.log("Finished GET");
 }
